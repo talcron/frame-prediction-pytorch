13c8bc4ad8688bc274d20f2de38ab5ca47ec3d5d
Ian Pegg <ianpegg9@gmail.com>
2021-02-15 19:14:25 -0800

    dataloader done

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,97 +1,77 @@
-from torch.utils.data import Dataset, DataLoader  # For custom data-sets
-import torchvision.transforms as transforms
-import torchvision.transforms.functional as TF
-import torchvision
 import numpy as np
-import os
-from PIL import Image
-import torch
-import pandas as pd
-from collections import namedtuple
-import io
+import torchvision.transforms as transforms
+from skvideo.io import vread
+from torch.utils.data import Dataset
 
-ORIG_HEIGHT = 126
-ORIG_WIDTH = 126
+DATASETS = {UCF101 := "UCF-101",
+            UCF_SPORTS := "ucf_sports"}
 
-# # a label and all meta information
-# Label = namedtuple('Label', [
-#     'name',
-#     'level3Id',
-#     'color',
-# ])
-#
-# labels = [
-#     #       name                     level3Id  color
-#     Label('road', 0, (128, 64, 128)),
-#     Label('drivable fallback', 1, (81, 0, 81)),
-#     Label('sidewalk', 2, (244, 35, 232)),
-#     Label('non-drivable fallback', 3, (152, 251, 152)),
-#     Label('person/animal', 4, (220, 20, 60)),
-#     Label('rider', 5, (255, 0, 0)),
-#     Label('motorcycle', 6, (0, 0, 230)),
-#     Label('bicycle', 7, (119, 11, 32)),
-#     Label('autorickshaw', 8, (255, 204, 54)),
-#     Label('car', 9, (0, 0, 142)),
-#     Label('truck', 10, (0, 0, 70)),
-#     Label('bus', 11, (0, 60, 100)),
-#     Label('vehicle fallback', 12, (136, 143, 153)),
-#     Label('curb', 13, (220, 190, 40)),
-#     Label('wall', 14, (102, 102, 156)),
-#     Label('fence', 15, (190, 153, 153)),
-#     Label('guard rail', 16, (180, 165, 180)),
-#     Label('billboard', 17, (174, 64, 67)),
-#     Label('traffic sign', 18, (220, 220, 0)),
-#     Label('traffic light', 19, (250, 170, 30)),
-#     Label('pole', 20, (153, 153, 153)),
-#     Label('obs-str-bar-fallback', 21, (169, 187, 214)),
-#     Label('building', 22, (70, 70, 70)),
-#     Label('bridge/tunnel', 23, (150, 100, 100)),
-#     Label('vegetation', 24, (107, 142, 35)),
-#     Label('sky', 25, (70, 130, 180)),
-#     Label('unlabeled', 26, (0, 0, 0)),
-# ]
-#
 
 class VideoDataset(Dataset):
 
-    def __init__(self, root_dir, index_file, video_frames=32, reshape_size=64):
-        self.num_frames = video_frames
-        self.reshape_size = reshape_size
-        self.data = pd.read_csv(os.path.join(root_dir, index_file))
+    def __init__(self, index_file, num_frames=32, shape=(64, 64), dataset=UCF101):
+        assert dataset in DATASETS, f"{dataset} not in {DATASETS}"
+        self.dataset = dataset
+        self.num_frames = num_frames
+        self.shape = shape
+        self.data = self._read_file(index_file)
 
-        # Define Transformations
-        self.PIL_transforms = transforms.Compose([transforms.ColorJitter(hue=hue_jitter, saturation=sat_jitter), ])
-        self.transforms = transforms.Compose([transforms.ToTensor(),
-                                                  transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)), ])
+        self.transform = self._define_transformation()
+
+    def stats(self):
+        # todo: verify a consistent timebase between videos. The UCF videos are all 1:10
+        filenames = self.data
+        stats = ('width', 'height', 'n_frames')
+        stats = {s: np.zeros((len(filenames),)) for s in stats}
+        for i, fn in enumerate(filenames):
+            video = self._read_video(fn)
+            n_frames, height, width, channels = video.shape
+            stats['n_frames'][i] = n_frames
+            stats['width'][i] = width
+            stats['height'][i] = height
+
+        print("Video Metadata:")
+        for k, v in stats.items():
+            print("{} | max: {} | min: {}".format(k, v.max(), v.min()))
+
+        return stats
+
+    @staticmethod
+    def _read_video(fn):
+        # noinspection PyTypeChecker
+        video = vread(fn, inputdict={'-s': '64x64'}, num_frames=32)
+        return video
+
+    @staticmethod
+    def _define_transformation():
+        to_tensor = transforms.ToTensor()
+
+        resnet_normalize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        standard_normalize = transforms.Normalize((0, 0, 0, 0), (1, 1, 1, 1))
+        return transforms.Compose([to_tensor, standard_normalize])
+
+    @staticmethod
+    def _read_file(index_file):
+        with open(index_file, 'r') as f:
+            data = f.read().split('\n')[:-1]
+        return data
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
+        fn = self.data[idx]
+        video = self._read_video(fn)
 
-        vid_name = self.data.iloc[idx, 0]
-        # img = Image.open(img_name).convert('RGB')
-        vid =
-        label_name = self.data.iloc[idx, 1]
-        label = Image.open(label_name)
+        video = self.transform(video)
+        label = self._label_from_path(fn)
+        return video, label
 
-        # Random Cropping of Image+Label
-        start_ind = [np.random.randint(ORIG_HEIGHT - IM_HEIGHT), np.random.randint(ORIG_WIDTH - IM_WIDTH)]
-        img = TF.crop(img, start_ind[0], start_ind[1], IM_HEIGHT, IM_WIDTH)
-        label = TF.crop(label, start_ind[0], start_ind[1], IM_HEIGHT, IM_WIDTH)
-
-        img = self.PIL_transforms(img)
-
-        img = np.asarray(img) / 255.  # scaling [0-255] values to [0-1]
-        label = np.asarray(label)
-
-        img = self.transforms(img).float()  # Normalization
-        label = torch.from_numpy(label.copy()).long()  # convert to tensor
-
-        # create one-hot encoding
-        # h, w = label.shape
-        # target = torch.zeros(self.n_class, h, w)
-        # for c in range(self.n_class):
-        #     target[c][label == c] = 1
-
-        return img, label
+    def _label_from_path(self, fn):
+        if self.dataset == UCF101:
+            label = fn.split('/')[-2]
+        elif self.dataset == UCF_SPORTS:
+            label = fn.split('/')[-3]
+        else:
+            raise ValueError(f"{self.dataset} not in {DATASETS}")
+        return label
