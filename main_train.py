06629563b2ab1b4087ba044ce3a4be387c89f290
Ian Pegg <ianpegg9@gmail.com>
2021-03-08 08:42:37 -0800

    organize arguments

--- main_train.py
+++ main_train.py
@@ -13,61 +13,74 @@ from runner import ImprovedVideoGAN
 def get_parser():
     global parser
     parser = argparse.ArgumentParser(description='VideoGAN')
-    parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
-                        help='number of data loading workers (default: 0)')
-    parser.add_argument('--num-gpu', default=1, type=int, help='number of GPUs to use')
-    parser.add_argument('--epochs', default=40, type=int, metavar='N',
-                        help='number of total epochs to run')
-    parser.add_argument('--arch', metavar='ARCH', default='basic_fcn', type=str,
-                        help='model architecture: ' + ' (default: basic_fcn)')
-    parser.add_argument('-b', '--batch-size', default=64, type=int,
-                        metavar='N', help='mini-batch size (default: 64)')
-    parser.add_argument('--lr', '--learning-rate', default=0.0001, type=float,
-                        metavar='LR', help='initial learning rate (default: 1e-4')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum')
-    parser.add_argument('--weight-decay', '--wd', default=0.0, type=float,
-                        metavar='W', help='weight decay (default: 0.0)')
-    parser.add_argument('--lr_step', default='40,60', help='decreasing strategy')
-    parser.add_argument('--print-freq', '-p', default=100, type=int,
-                        metavar='N', help='print frequency (default: 10)')
-    parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                        help='path to latest checkpoint (default: none)')
-    parser.add_argument('--exp-key', default='', type=str,
-                        help='The key to an existing experiment to be continued (default: None)')
-    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                        help='evaluate model on test set')
-    parser.add_argument('--pretrained', dest='pretrained', action='store_true',
-                        help='use pre-trained model')
-    parser.add_argument('--num-frozen', default=0, type=int, metavar='N',
-                        help='# frozen cnv2 layers')
-    parser.add_argument('--root-dir', default='/', type=str, metavar='PATH',
-                        help='directory containing videos and index file (default: /)')
-    parser.add_argument('--index-file', default='mjpeg-index.txt', type=str, metavar='FILENAME',
-                        help='index file referencing all videos relative to root_dir (default: mjpeg-index.txt)')
-    parser.add_argument('--save-dir', default='extra/', type=str, metavar='PATH',
-                        help='path to directory for saved outputs (default: extra/)')
-    parser.add_argument('--color-sat', default=0.0, type=float,
-                        help='factor for saturation jitter transform (default: 0.0)')
-    parser.add_argument('--color-hue', default=0.0, type=float,
-                        help='factor for hue jitter transform (default: 0.0)')
-    parser.add_argument('--beta1', default=0.5, type=float,
-                        help='Beta parameter for ADAM (default: 0.5)')
-    parser.add_argument('--zdim', default=100, type=int,
-                        help='Dimensionality of hidden features (default: 100)')
-    parser.add_argument('--exp-name', default='dev', type=str, nargs='+',
-                        help='The experiment name (default: deb)')
-    parser.add_argument('--exp-disable', default=False, action='store_true',
-                        help='Disable CometML (default: False if switch is absent)')
-    parser.add_argument('--cache-dataset', default=False, action='store_true',
-                        help='Keep all data in memory (default: False if switch is absent)')
-    parser.add_argument('--spec-norm', default=False, action='store_true',
-                        help='set to True to use spectral normalization')
-    parser.add_argument('--no-gp', default=False, action='store_true',
-                        help='set to True to stop using Gradient Penalty')
-    parser.add_argument('--drift-penalty', default=False, action='store_true',
-                        help='set to True to use small drift penalty (prevents discriminator loss drifting)')
 
+    run_args = parser.add_argument_group('Run', "Run options")
+    run_args.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                          help='evaluate model on test set')
+    run_args.add_argument('--root-dir', default='/', type=str, metavar='PATH',
+                          help='directory containing videos and index file (default: /)')
+    run_args.add_argument('--index-file', default='mjpeg-index.txt', type=str, metavar='FILENAME',
+                          help='index file referencing all videos relative to root_dir (default: mjpeg-index.txt)')
+    run_args.add_argument('--save-dir', default='extra/', type=str, metavar='PATH',
+                          help='path to directory for saved outputs (default: extra/)')
+    run_args.add_argument('--resume', default='', type=str, metavar='PATH',
+                          help='path to latest checkpoint (default: none)')
+    run_args.add_argument('--epochs', default=40, type=int, metavar='N',
+                          help='number of total epochs to run')
+
+    learner_args = parser.add_argument_group("Learner")
+    learner_args.add_argument('--spec-norm', default=False, action='store_true',
+                              help='set to True to use spectral normalization')
+    learner_args.add_argument('--no-gp', default=False, action='store_true',
+                              help='set to True to stop using Gradient Penalty')
+    learner_args.add_argument('--drift-penalty', default=False, action='store_true',
+                              help='set to True to use small drift penalty (prevents discriminator loss drifting)')
+
+    compute_args = parser.add_argument_group("Compute")
+    compute_args.add_argument('-j', '--workers', default=0, type=int, metavar='N',
+                              help='number of data loading workers (default: 0)')
+    compute_args.add_argument('--num-gpu', default=1, type=int, help='number of GPUs to use')
+    compute_args.add_argument('--cache-dataset', default=False, action='store_true',
+                              help='Keep all data in memory (default: False if switch is absent)')
+
+    model_args = parser.add_argument_group("Model")
+    model_args.add_argument('--arch', metavar='ARCH', default='basic_fcn', type=str,
+                            help='model architecture: ' + ' (default: basic_fcn)')
+    model_args.add_argument('--pretrained', dest='pretrained', action='store_true',
+                            help='use pre-trained model')
+    model_args.add_argument('--num-frozen', default=0, type=int, metavar='N',
+                            help='# frozen cnv2 layers')
+    model_args.add_argument('--zdim', default=100, type=int,
+                            help='Dimensionality of hidden features (default: 100)')
+
+    optimizer_args = parser.add_argument_group("Optimizer")
+    optimizer_args.add_argument('-b', '--batch-size', default=64, type=int,
+                                metavar='N', help='mini-batch size (default: 64)')
+    optimizer_args.add_argument('--lr', '--learning-rate', default=0.0001, type=float,
+                                metavar='LR', help='initial learning rate (default: 1e-4')
+    optimizer_args.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                                help='momentum')
+    optimizer_args.add_argument('--weight-decay', '--wd', default=0.0, type=float,
+                                metavar='W', help='weight decay (default: 0.0)')
+    optimizer_args.add_argument('--lr_step', default='40,60', help='decreasing strategy')
+    optimizer_args.add_argument('--beta1', default=0.5, type=float,
+                                help='Beta parameter for ADAM (default: 0.5)')
+
+    pre_process_args = parser.add_argument_group("Pre-process")
+    pre_process_args.add_argument('--color-sat', default=0.0, type=float,
+                                  help='factor for saturation jitter transform (default: 0.0)')
+    pre_process_args.add_argument('--color-hue', default=0.0, type=float,
+                                  help='factor for hue jitter transform (default: 0.0)')
+
+    log_args = parser.add_argument_group("Log")
+    log_args.add_argument('--print-freq', '-p', default=100, type=int,
+                          metavar='N', help='print frequency (default: 10)')
+    log_args.add_argument('--exp-name', default='dev', type=str, nargs='+',
+                          help='The experiment name (default: deb)')
+    log_args.add_argument('--exp-key', default='', type=str,
+                          help='The key to an existing experiment to be continued (default: None)')
+    log_args.add_argument('--exp-disable', default=False, action='store_true',
+                          help='Disable CometML (default: False if switch is absent)')
     return parser
 
 
